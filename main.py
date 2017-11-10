10051e4774c486035070e027df99736ecdd3a78f
ricott1 <ricott2@gmail.com>
2017-11-10 20:44:30 +0100

    working?

--- main.py
+++ main.py
@@ -3,74 +3,80 @@ import sys, curses, time, copy
 
 
 def print_state(stdscr, p, g, runners, GOAL, steps, best):
-	stdscr.addstr(0, 0, "time {:3}  pool {}    generation {}   best {}  ".format(steps, p, g, best))
-	for i, r in enumerate(runners):
-		stdscr.addstr(i+1, 0, r.print_state() + "  :" + "|" * int(r.position) + " " * (GOAL - int(r.position)) + ":GOAL")
-	stdscr.refresh()
+    stdscr.addstr(0, 0, "time {:3}  pool {}    generation {}   best {}  ".format(steps, p, g, best))
+    for i, r in enumerate(runners):
+        stdscr.addstr(i+1, 0, r.print_state() + "  :" + "|" * int(r.distance/256.) + " " * (100))#
+    stdscr.refresh()
 
 def main():
-	stdscr = curses.initscr()
-	curses.noecho()
-	curses.cbreak()
-	N = 20
-	GOAL = 50
-	GENERATIONS = 50
-	mutation_rate = 0.5
-	structure = [3 * N, 10, 1]
-	winners = []
-	WIN_CONDITION = 0
-	best = 999
-	try:
-		for g in xrange(GENERATIONS):
-			#GOAL += 1
-			TIME_OUT = 10 * GOAL
-			pool = [[Runner(j, p, g, 1, structure) for j in xrange(N)] for p in xrange(N)]
-			if winners:
-				for p, runners in enumerate(pool):
-					for i, r in enumerate(runners):
-						runners[i].brain = copy.deepcopy(winners[i].brain)
-						runners[i].id += winners[i].id
-						
-						runners[i].brain.mutate(mutation_rate * p / g / N)
-			winners = []
-			for p, runners in enumerate(pool):
-				arrived = 0
-				steps = 0
-				start = time.time()
-				while True:
-					state = ""
-					
-					
-					for j, r in enumerate(sorted([run for run in runners], key= lambda x: x.position, reverse=True)):
-						#r.recover(j)
-						if not r.isArrived:
-							inputs = [r.position + 1, r.velocity + 1, r.energy + 1] + [j  for i in [[s.position + 1, s.velocity + 1, s.energy + 1] for s in runners if s != r] for j in i] 
-							r.update(inputs)
-						if r.position >= GOAL and not r.isArrived:
-							arrived += 1
-							r.isArrived = arrived
-					steps += 1
-					print_state(stdscr, p, g, runners, GOAL, steps, best)
-					if arrived >= 1 + WIN_CONDITION or steps > TIME_OUT:
-						winner = sorted([r for r in runners], key= lambda x: x.position, reverse=True)[WIN_CONDITION]
-						winners.append(winner)
-						best = min(1.*steps/GOAL, best)
-						break
-					#time.sleep(0.11)
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    
+    
+    structure = [4, 10, 2]
+    winners = []
+    
+    best = 999
+    try:
+        for g in xrange(GENERATIONS):
+            #GOAL += 1
+            start_tach, frame_count = 21, 0
+            pool = [[Runner(j, p, g, structure, start_tach, frame_count) for j in xrange(N)] for p in xrange(N)]
+            if winners:
+                evolve_runners(pool, g, winners)                
+            winners = []
+            for p, runners in enumerate(pool):
+                winner, best = run_pool(p, g, runners, best, stdscr)
+                winners.append(winner)            
+    finally:
+        curses.echo()
+        curses.nocbreak()
+        
+        raw_input()
+        curses.endwin()
+        print best
 
-			
+def evolve_runners(pool, g, winners):
+    for p, runners in enumerate(pool):
+        for i, r in enumerate(runners):
+            runners[i].brain = copy.deepcopy(winners[i].brain)
+            runners[i].id += winners[i].id
+            runners[i].brain.mutate(MUTATION_RATE * p / g / N)
 
-			
-	finally:
-		curses.echo()
-		curses.nocbreak()
-		
-		raw_input()
-		curses.endwin()
-		print best
+def run_pool(p, g, runners, best, stdscr):
+    arrived = 0
+    steps = 0
+    start = time.time()
+    while True:
+        state = ""
+        for j, r in enumerate(sorted([run for run in runners], key= lambda x: x.distance, reverse=True)):
+            #r.recover(j)
+            if not r.isArrived:
+                inputs = [r.frame, r.distance, r.tach, r.speed]
+                r.update(inputs)
+            if r.distance >= GOAL and not r.isArrived and not r.blowup:
+                arrived += 1
+                r.isArrived = arrived
+            #time.sleep(0.15)    
+        steps += 1
+        print_state(stdscr, p, g, runners, GOAL, steps, best)
+        if arrived >= 1 + WIN_CONDITION or steps > 2 * best/0.0334:
+            winner = sorted([r for r in runners], key= lambda x: x.distance, reverse=True)[WIN_CONDITION]
+            print winner.time
+            if winner.time < best:
+            	#with open("champion.drag", "w") as f:
+            	#	f.write() 
+            	best = winner.time
+            break
 
+    return winner, best
 
-
-
+N = 50
+GOAL = 97 * 256
+TIME_OUT = 20/0.0334
+GENERATIONS = 20
+WIN_CONDITION = 0
+MUTATION_RATE = 0.5
 if __name__ == '__main__':
-	main()
+    main()
--- runner.py
+++ runner.py
@@ -1,48 +1,131 @@
-import random
+import random, math
 import brain
 
 
 class Runner(object):
-	def __init__(self, index, pool, generation, max_energy, structure):
-		self.index = index
-		self.pool = pool
-		self.generation = generation
-		self.id = str(self.pool)
-		self.max_energy = max_energy
-		self.energy = self.max_energy
+    def __init__(self, index, pool, generation, structure, start_tach, frame_count):
+        self.index = index
+        self.pool = pool
+        self.generation = generation
+        self.id = str(self.pool)
+        
+        
+        self.distance = 0
+        self.speed = 0
+        self.gear = 0
+        self.gas = 1
+        self.tach = start_tach
+        self.advance = 1
+        self.frame = frame_count
+        self.limit = self.get_initial_limit()
+        self.posttach = self.get_initial_posttach()
+        self.shift = 1
 
-		self.velocity = 0
-		self.position = 0
-		self.output = 0
-		self.isArrived = 0
+        self.output = 0
+        self.isArrived = 0
+        self.time = 0
+        self.blowup = 0
+        self.brain = brain.Brain(structure)
 
-		self.brain = brain.Brain(structure)
+    def update(self, inputs):
+    	if self.blowup == 0:
+	        results = self.brain.propagate(inputs, 0)
+	        #print results
+	        self.gear = self.update_gear()
+	        #print "gear is {}".format(self.gear)
+	        self.gas = int(results[0] > 0.5)
 
-	def update(self, inputs):
-		self.output = self.brain.propagate(inputs, 0)[0]
-		self.position += self.velocity
-		
-		if self.energy  - (self.velocity + self.output) > 0:
-			self.velocity = max(0, min(self.max_energy, self.velocity + self.output))
-			self.energy = min(self.max_energy, self.energy - self.velocity)
-		elif self.energy  - self.velocity > 0:
-			self.velocity = max(0, min(self.max_energy, self.velocity))
-			self.energy = min(self.max_energy, self.energy - self.velocity)
-		else:
-			self.max_energy = max(0, self.max_energy - 0.01)
-			self.velocity = max(0, min(self.max_energy, self.velocity))
-			self.energy = min(self.max_energy, max(0, self.energy + 0.01))
+	        self.distance = self.update_distance()
 
-	def recover(self, j):
-		bonus = max(0, 0.0005 * (10 - j))
-		self.max_energy = min(1, self.max_energy + bonus)
+	        self.frame = self.update_frame()
+	        self.advance = self.update_advance()
+	        self.tach = self.update_tach()
+	        self.limit = self.update_limit()
+	        self.posttach = self.update_posttach()
+	        
+	        self.speed = self.update_speed()
+	        self.shift = int(results[1] > 0.5)
+	        self.time += 0.0334
 
-	def print_state(self):
-		return "{:5s}: x = {:6.2f}; v = {:.2f}; e = {:4.2f}/{:4.2f}".format(self.id[-5:], f(self.position), f(self.velocity), f(self.energy), f(self.max_energy) )
+	        if self.tach >= 32:
+	        	self.blowup = 1
+        
+        
+        
+        
+        self.posttach = self.update_posttach()
 
+    def get_initial_limit(self):
+        return self.update_limit()
 
+    def get_initial_posttach(self):
+        if self.limit - self.speed >= 16:
+            return self.tach - 1
+        else:
+            return self.tach
 
-def f(value):
-	return round(value, 2)
+    def update_gear(self):
+        return min(self.gear + self.shift, 4)
 
+    def update_distance(self):
+        return self.distance + self.speed
 
+    def update_frame(self):
+        return self.frame + 2
+
+    def update_advance(self):
+        if self.frame % 2**(self.gear) == 0:
+            return 1
+        else:
+            return 0
+
+    def update_tach(self):
+        if self.shift == 1:
+            if self.gas == 1:
+                return self.posttach + 3
+            else:
+                return self.posttach - 3
+        else:
+            if self.advance == 1:
+                if self.gas == 1:
+                    return self.posttach + 1
+                else:
+                    return self.posttach - 1
+            else:
+                return self.posttach
+
+    def update_posttach(self):
+        if self.shift == 1:
+            return self.tach
+        else:
+            if self.limit - self.speed >= 16:
+                return self.tach - 1
+            else:
+                return self.tach
+
+    def update_limit(self):
+        if self.tach >= 20:
+            limit = self.tach * 2**(self.gear - 1) + 2**(self.gear - 2)
+        else:
+            limit = self.tach * 2**(self.gear - 1)
+        return math.floor(limit)
+
+    def update_speed(self):
+        if self.gear > 0:
+            if self.shift == 1:
+                return self.speed
+            else:
+                if self.speed < self.limit:
+                    return self.speed + 2
+                elif self.speed == self.limit:
+                    return self.speed
+                else:
+                    return self.speed - 1
+        else:
+            return 0
+
+    def print_state(self):
+    	#print "{}: {} d = {}; s = {}; g = {}".format(self.id[-5:], self.frame, self.distance, self.speed, self.gear)
+    	#raw_input()
+        return "{:1s}: {:4.2f} d={:3d}; s={:2d}; g={:1d} gas={:1d} sh={:1d}".format("B" * self.blowup, round(self.time,2), int(self.distance/256), self.speed,
+        															self.gear, self.gas, self.shift)
